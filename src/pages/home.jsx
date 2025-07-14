cb194a1c74f184a0ad6c6aef3da78913b56e019c
Ram Lakhan Jangid <rljangid825@gmail.com>
2025-07-14 13:25:04 +0530

    ram lakhan jangid

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,20 @@
-import About from "../components/About.jsx";
-import Bad from "../components/Bad.jsx";
-import Begining from "../components/Begining.jsx";
-import Blogs from "../components/Blogs.jsx";
-import Connect from "../components/Connect.jsx";
-import Contact from "../components/Contact.jsx";
-import Footer from "../components/Footer.jsx";
-import Hero from "../components/Hero.jsx";
-import Legacy from "../components/Legacy.jsx";
-import Media from "../components/Media.jsx";
-import Podcasts from "../components/Podcasts.jsx";
-import Result from "../components/Result.jsx";
-import Services from "../components/Services.jsx";
-import Struggle from "../components/Struggle.jsx";
-import Success from "../components/Success.jsx";
-import Testimonials from "../components/Testimonials.jsx";
-import Why from "../components/Why.jsx";
+import About from "../components/about.jsx";
+import Bad from "../components/bad.jsx";
+import Begining from "../components/begining.jsx";
+import Blogs from "../components/blogs.jsx";
+import Connect from "../components/connect.jsx";
+import Contact from "../components/contact.jsx";
+import Footer from "../components/footer.jsx";
+import Hero from "../components/hero.jsx";
+import Legacy from "../components/legacy.jsx";
+import Media from "../components/media.jsx";
+import Podcasts from "../components/podcasts.jsx";
+import Result from "../components/result.jsx";
+import Services from "../components/services.jsx";
+import Struggle from "../components/struggle.jsx";
+import Success from "../components/success.jsx";
+import Testimonials from "../components/testimonials.jsx";
+import Why from "../components/why.jsx";
 
 
 
